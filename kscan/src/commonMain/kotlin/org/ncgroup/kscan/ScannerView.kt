ed97e39417286c1eb2939ba798becee38e0190dc
Chris Gonzales <chrisg32@users.noreply.github.com>
2025-04-01 21:55:03 -0500

    Added modifier and showUi parameters

--- kscan/src/commonMain/kotlin/org/ncgroup/kscan/ScannerView.kt
+++ kscan/src/commonMain/kotlin/org/ncgroup/kscan/ScannerView.kt
@@ -1,10 +1,14 @@
 package org.ncgroup.kscan
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 expect fun ScannerView(
+    modifier: Modifier = Modifier.fillMaxSize(),
     codeTypes: List<BarcodeFormat>,
     colors: ScannerColors = scannerColors(),
+    showUi: Boolean = true,
     result: (BarcodeResult) -> Unit,
 )
--- kscan/src/wasmJsMain/kotlin/org/ncgroup/kscan/ScannerView.wasmJs.kt
+++ kscan/src/wasmJsMain/kotlin/org/ncgroup/kscan/ScannerView.wasmJs.kt
@@ -1,10 +1,13 @@
 package org.ncgroup.kscan
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 actual fun ScannerView(
+    modifier: Modifier,
     codeTypes: List<BarcodeFormat>,
     colors: ScannerColors,
+    showUi: Boolean,
     result: (BarcodeResult) -> Unit,
 ) {}
